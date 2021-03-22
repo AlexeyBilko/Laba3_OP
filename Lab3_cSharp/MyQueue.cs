955b76a4a65823ade6d50d91de1a801043830ef3
Alexey Bilko <bilkoalexey67@gmail.com>
2021-03-22 12:33:35 +0200

    Add project files.

---
+++ Lab3_cSharp/MyQueue.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_cSharp
+{
+    public class MyQueue<T>
+    {
+        private T[] _array;
+        private int size;
+        private const int defaultCapacity = 10;
+        private int capacity;
+        private int head;
+        private int tail;
+
+        public MyQueue()
+        {
+            capacity = defaultCapacity;
+            this._array = new T[defaultCapacity];
+            this.size = 0;
+            this.head = -1;
+            this.tail = 0;
+        }
+
+        public bool isEmpty()
+        {
+            return size == 0;
+        }
+
+        public void Enqueue(T newElement)
+        {
+            if (this.size == this.capacity)
+            {
+                T[] newQueue = new T[2 * capacity];
+                Array.Copy(_array, 0, newQueue, 0, _array.Length);
+                _array = newQueue;
+                capacity *= 2;
+            }
+            size++;
+            _array[tail++ % capacity] = newElement;
+        }
+
+        public T Dequeue()
+        {
+            if (this.size == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            size--;
+            return _array[++head % capacity];
+        }
+
+
+        public int Count
+        {
+            get
+            {
+                return this.size;
+            }
+        }
+    }
+}
